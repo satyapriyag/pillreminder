f8527d04b3e7807ffe1c5d1be3f2df4a85873b93
Satya <satya.priya@practo.com>
2016-10-26 17:41:43 +0530

    initializer for web logic

--- src/main/java/pills/Application.java
+++ src/main/java/pills/Application.java
@@ -3,6 +3,8 @@ package pills;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @ComponentScan
 @SpringBootApplication
 @EnableScheduling
-public class Application  {
+public class Application extends SpringBootServletInitializer  {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
