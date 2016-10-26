5ab7760e8532fc99552de5096b4883253892d325
Satya <satya.priya@practo.com>
2016-10-26 18:03:07 +0530

    adding configuration for application class

--- src/main/java/pills/Application.java
+++ src/main/java/pills/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @ComponentScan
 @SpringBootApplication
 @EnableScheduling
+@Configuration
 public class Application extends SpringBootServletInitializer  {
 
 	@Override
